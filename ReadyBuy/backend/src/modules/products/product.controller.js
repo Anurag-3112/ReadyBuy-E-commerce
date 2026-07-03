cb08138980f567e8660621764b0c4f0763339083
Anurag Kumar <ak7546824@gmail.com>
2026-07-04 01:12:26 +0530

    Create product.controller.js

---
+++ ReadyBuy/backend/src/modules/products/product.controller.js
@@ -0,0 +1,85 @@
+import asyncHandler from "../../shared/utils/asyncHandler.js";
+import redis from "../../config/redis.js";
+
+import {
+    createProductService,
+    getProductsService,
+    getProductBySlugService,
+    updateProductService,
+    deleteProductService,
+} from "./product.service.js";
+
+export const createProduct = asyncHandler(
+    async (req, res) => {
+        const product =
+            await createProductService(
+                req.body,
+                req.files
+            );
+
+        return res.status(201).json({
+            success: true,
+            message: "Product created",
+            data: product,
+        });
+    }
+);
+
+export const getProducts = asyncHandler(
+    async (req, res) => {
+        const result =
+            await getProductsService(
+                req.query
+            );
+
+        return res.status(200).json({
+            success: true,
+            message:
+                "Products fetched successfully",
+            data: result,
+        });
+    }
+);
+
+export const getProductBySlug =
+    asyncHandler(async (req, res) => {
+        const product =
+            await getProductBySlugService(
+                req.params.slug
+            );
+
+        return res.status(200).json({
+            success: true,
+            message: "Product fetched",
+            data: product,
+        });
+    });
+
+export const updateProduct =
+    asyncHandler(async (req, res) => {
+        const product =
+            await updateProductService(
+                req.params.id,
+                req.body
+            );
+
+        return res.status(200).json({
+            success: true,
+            message:
+                "Product updated successfully",
+            data: product,
+        });
+    });
+
+export const deleteProduct =
+    asyncHandler(async (req, res) => {
+        await deleteProductService(
+            req.params.id
+        );
+
+        return res.status(200).json({
+            success: true,
+            message:
+                "Product deleted successfully",
+        });
+    });
