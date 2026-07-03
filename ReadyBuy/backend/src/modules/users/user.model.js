c087c5b06fe009d121937374929b66fc1b9dca65
Anurag Kumar <ak7546824@gmail.com>
2026-07-04 01:12:44 +0530

    Create user.model.js

---
+++ ReadyBuy/backend/src/modules/users/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+
+        password: {
+            type: String,
+            required: true,
+            select: false,
+        },
+
+        role: {
+            type: String,
+            enum: ["USER", "ADMIN"],
+            default: "USER",
+        },
+
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+
+        refreshToken: {
+            type: String,
+            default: null,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
