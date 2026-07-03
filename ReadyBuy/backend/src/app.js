fbffbc929f9328e8c943c5995a406fabc0a90492
Anurag Kumar <ak7546824@gmail.com>
2026-07-04 01:10:50 +0530

    Create app.js

---
+++ ReadyBuy/backend/src/app.js
@@ -0,0 +1,57 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import notFound from "./shared/middleware/notFound.middleware.js";
+import errorHandler from "./shared/middleware/error.middleware.js";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+
+import healthRoutes from "./routes/health.routes.js";
+import authRoutes from "./modules/auth/auth.routes.js";
+import productRoutes from "./modules/products/product.routes.js";
+import cartRoutes from "./modules/carts/cart.routes.js";
+import orderRoutes from "./modules/orders/order.routes.js";
+
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec, } from "./config/swagger.js";
+
+import adminRoutes from "./modules/admin/admin.routes.js";
+import categoryRoutes from "./modules/categories/category.routes.js";
+import dashboardRoutes from "./modules/dashboard/dashboard.routes.js";
+
+const app = express();
+app.use(helmet());
+
+app.use(
+    cors({
+        origin: true,
+        credentials: true,
+    })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
+
+// Routes
+app.use("/api/v1/health", healthRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/cart", cartRoutes);
+app.use("/api/v1/orders", orderRoutes);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1/categories", categoryRoutes);
+app.use("/api/v1/admin/dashboard", dashboardRoutes);
+
+// 404 handler
+app.use(notFound);
+
+// Global error handler
+app.use(errorHandler);
+
+export default app;
