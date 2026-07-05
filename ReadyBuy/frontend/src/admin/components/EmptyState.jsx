3ea2a004d06393147ee6fa4e148868bd7c1f9698
Anurag Kumar <ak7546824@gmail.com>
2026-07-05 20:33:50 +0530

    Create EmptyState.jsx

---
+++ ReadyBuy/frontend/src/admin/components/EmptyState.jsx
@@ -0,0 +1,20 @@
+const EmptyState = ({
+    title,
+    subtitle,
+}) => {
+
+    return (
+
+        <div className="text-center py-5">
+
+            <h4>{title}</h4>
+
+            <p>{subtitle}</p>
+
+        </div>
+
+    );
+
+};
+
+export default EmptyState;
