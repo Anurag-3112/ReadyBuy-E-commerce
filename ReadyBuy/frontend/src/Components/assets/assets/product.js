e9d96a1993d9911af22c1e795e9c7715b89d6bfd
Anurag Kumar <ak7546824@gmail.com>
2026-07-05 20:36:33 +0530

    Create product.js

---
+++ ReadyBuy/frontend/src/Components/assets/assets/product.js
@@ -0,0 +1,278 @@
+// ================= BOYS =================
+import p1_1 from "./p1_1.png";
+import p1_2 from "./p1_2.png";
+import p1_3 from "./p1_3.png";
+import p1_4 from "./p1_4.png";
+
+import p2_1 from "./p2_1.png";
+import p2_2 from "./p2_2.png";
+import p2_3 from "./p2_3.png";
+import p2_4 from "./p2_4.png";
+
+import p3_1 from "./p3_1.png";
+import p3_2 from "./p3_2.png";
+import p3_3 from "./p3_3.png";
+import p3_4 from "./p3_4.png";
+
+import p4_1 from "./p4_1.png";
+import p4_2 from "./p4_2.png";
+import p4_3 from "./p4_3.png";
+import p4_4 from "./p4_4.png";
+
+import p5_1 from "./p5_1.png";
+import p5_2 from "./p5_2.png";
+import p5_3 from "./p5_3.png";
+import p5_4 from "./p5_4.png";
+
+// ================= GIRLS =================
+import p6_1 from "./p6_1.png";
+import p6_2 from "./p6_2.png";
+import p6_3 from "./p6_3.png";
+import p6_4 from "./p6_4.png";
+
+import p7_1 from "./p7_1.png";
+import p7_2 from "./p7_2.png";
+import p7_3 from "./p7_3.png";
+import p7_4 from "./p7_4.png";
+
+import p8_1 from "./p8_1.png";
+import p8_2 from "./p8_2.png";
+import p8_3 from "./p8_3.png";
+import p8_4 from "./p8_4.png";
+
+import p9_1 from "./p9_1.png";
+import p9_2 from "./p9_2.png";
+import p9_3 from "./p9_3.png";
+import p9_4 from "./p9_4.png";
+
+import p10_1 from "./p10_1.png";
+import p10_2 from "./p10_2.png";
+import p10_3 from "./p10_3.png";
+import p10_4 from "./p10_4.png";
+
+// ================= MEN =================
+import p11_1 from "./p11_1.png";
+import p11_2 from "./p11_2.png";
+import p11_3 from "./p11_3.png";
+import p11_4 from "./p11_4.png";
+
+import p12_1 from "./p12_1.png";
+import p12_2 from "./p12_2.png";
+import p12_3 from "./p12_3.png";
+import p12_4 from "./p12_4.png";
+
+import p13_1 from "./p13_1.png";
+import p13_2 from "./p13_2.png";
+import p13_3 from "./p13_3.png";
+import p13_4 from "./p13_4.png";
+
+import p14_1 from "./p14_1.png";
+import p14_2 from "./p14_2.png";
+import p14_3 from "./p14_3.png";
+import p14_4 from "./p14_4.png";
+
+import p15_1 from "./p15_1.png";
+import p15_2 from "./p15_2.png";
+import p15_3 from "./p15_3.png";
+import p15_4 from "./p15_4.png";
+
+// ================= WOMEN =================
+import p16_1 from "./p16_1.png";
+import p16_2 from "./p16_2.png";
+import p16_3 from "./p16_3.png";
+import p16_4 from "./p16_4.png";
+
+import p17_1 from "./p17_1.png";
+import p17_2 from "./p17_2.png";
+import p17_3 from "./p17_3.png";
+import p17_4 from "./p17_4.png";
+
+import p18_1 from "./p18_1.png";
+import p18_2 from "./p18_2.png";
+import p18_3 from "./p18_3.png";
+import p18_4 from "./p18_4.png";
+
+import p19_1 from "./p19_1.png";
+import p19_2 from "./p19_2.png";
+import p19_3 from "./p19_3.png";
+import p19_4 from "./p19_4.png";
+
+import p20_1 from "./p20_1.png";
+import p20_2 from "./p20_2.png";
+import p20_3 from "./p20_3.png";
+import p20_4 from "./p20_4.png";
+
+
+const product = [
+  // ================= kids =================
+  {
+    id: 1,
+    category: "kids",
+    name: "Boys Orange Coloured Shirt",
+    images: [p1_1, p1_2, p1_3, p1_4],
+    new_price: 1999,
+    old_price: 2999,
+  },
+  {
+    id: 2,
+    category: "kids",
+    name: "Boys Blue Denim Shirt",
+    images: [p2_1, p2_2, p2_3, p2_4],
+    new_price: 1799,
+    old_price: 2599,
+  },
+  {
+    id: 3,
+    category: "kids",
+    name: "Boys Casual White T-Shirt",
+    images: [p3_1, p3_2, p3_3, p3_4],
+    new_price: 999,
+    old_price: 1499,
+  },
+  {
+    id: 4,
+    category: "kids",
+    name: "Boys Printed Hoodie",
+    images: [p4_1, p4_2, p4_3, p4_4],
+    new_price: 2499,
+    old_price: 3299,
+  },
+  {
+    id: 5,
+    category: "kids",
+    name: "Boys Checked Cotton Shirt",
+    images: [p5_1, p5_2, p5_3, p5_4],
+    new_price: 1899,
+    old_price: 2699,
+  },
+
+  // ================= GIRLS =================
+  {
+    id: 6,
+    category: "kids",
+    name: "Girls Pink Floral Dress",
+    images: [p6_1, p6_2, p6_3, p6_4],
+    new_price: 2299,
+    old_price: 3199,
+  },
+  {
+    id: 7,
+    category: "kids",
+    name: "Girls Denim Jacket",
+    images: [p7_1, p7_2, p7_3, p7_4],
+    new_price: 2799,
+    old_price: 3599,
+  },
+  {
+    id: 8,
+    category: "kids",
+    name: "Girls Casual Yellow Top",
+    images: [p8_1, p8_2, p8_3, p8_4],
+    new_price: 1199,
+    old_price: 1699,
+  },
+  {
+    id: 9,
+    category: "kids",
+    name: "Girls Printed Skirt",
+    images: [p9_1, p9_2, p9_3, p9_4],
+    new_price: 1499,
+    old_price: 2199,
+  },
+  {
+    id: 10,
+    category: "kids",
+    name: "Girls Party Wear Frock",
+    images: [p10_1, p10_2, p10_3, p10_4],
+    new_price: 2999,
+    old_price: 3999,
+  },
+
+  // ================= MEN =================
+  {
+    id: 11,
+    category: "men",
+    name: "Men Blue Denim Jeans",
+    images: [p11_1, p11_2, p11_3, p11_4],
+    new_price: 2499,
+    old_price: 3499,
+  },
+  {
+    id: 12,
+    category: "men",
+    name: "Men Slim Fit Black Shirt",
+    images: [p12_1, p12_2, p12_3, p12_4],
+    new_price: 2199,
+    old_price: 2999,
+  },
+  {
+    id: 13,
+    category: "men",
+    name: "Men Casual White T-Shirt",
+    images: [p13_1, p13_2, p13_3, p13_4],
+    new_price: 999,
+    old_price: 1499,
+  },
+  {
+    id: 14,
+    category: "men",
+    name: "Men Winter Jacket",
+    images: [p14_1, p14_2, p14_3, p14_4],
+    new_price: 3999,
+    old_price: 5299,
+  },
+  {
+    id: 15,
+    category: "men",
+    name: "Men Party Wear Blazer",
+    images: [p15_1, p15_2, p15_3, p15_4],
+    new_price: 5999,
+    old_price: 7499,
+  },
+
+  // ================= WOMEN =================
+  {
+    id: 16,
+    category: "women",
+    name: "Women Floral Summer Dress",
+    images: [p16_1, p16_2, p16_3, p16_4],
+    new_price: 2799,
+    old_price: 3599,
+  },
+  {
+    id: 17,
+    category: "women",
+    name: "Women Pink Cotton Kurti",
+    images: [p17_1, p17_2, p17_3, p17_4],
+    new_price: 1999,
+    old_price: 2799,
+  },
+  {
+    id: 18,
+    category: "women",
+    name: "Women Blue Denim Jeans",
+    images: [p18_1, p18_2, p18_3, p18_4],
+    new_price: 2599,
+    old_price: 3399,
+  },
+  {
+    id: 19,
+    category: "women",
+    name: "Women Party Wear Gown",
+    images: [p19_1, p19_2, p19_3, p19_4],
+    new_price: 6999,
+    old_price: 8999,
+  },
+  {
+    id: 20,
+    category: "women",
+    name: "Women Casual Cotton Saree",
+    images: [p20_1, p20_2, p20_3, p20_4],
+    new_price: 3499,
+    old_price: 4599,
+  },
+
+  // repeat same pattern until id 50
+];
+
+export default product;
