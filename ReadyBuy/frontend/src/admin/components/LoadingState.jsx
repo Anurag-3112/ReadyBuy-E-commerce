54d9e65ada794f3eab0d53c5214795635f8e9176
Anurag Kumar <ak7546824@gmail.com>
2026-07-05 20:33:57 +0530

    Create LoadingState.jsx

---
+++ ReadyBuy/frontend/src/admin/components/LoadingState.jsx
@@ -0,0 +1,13 @@
+import Spinner from "react-bootstrap/Spinner";
+
+const LoadingState = () => (
+
+    <div className="text-center py-5">
+
+        <Spinner />
+
+    </div>
+
+);
+
+export default LoadingState;
