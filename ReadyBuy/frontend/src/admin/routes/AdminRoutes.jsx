53e8d22b5d5dd6f478635e74d9823ce91676de38
Anurag Kumar <ak7546824@gmail.com>
2026-07-05 20:35:46 +0530

    Update AdminRoutes.jsx

--- ReadyBuy/frontend/src/admin/routes/AdminRoutes.jsx
+++ ReadyBuy/frontend/src/admin/routes/AdminRoutes.jsx
@@ -1,33 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 
+import AdminLayout from "../layout/AdminLayout";
 import Dashboard from "../pages/Dashboard";
 import ProductList from "../products/ProductList";
+import OrdersList from "../orders/OrdersList";
+import CategoriesList from "../categories/CategoriesList";
+import UsersList from "../users/UsersList";
 
 const AdminRoutes = () => {
-
     return (
-
         <Routes>
-
-            <Route
-                index
-                element={<Dashboard />}
-            />
-
-            <Route
-                path="products"
-                element={
-                    <ProductList />
-                }
-            />
-
+            <Route element={<AdminLayout />}>
+                <Route index element={<Dashboard />} />
+                <Route path="products" element={<ProductList />} />
+                <Route path="orders" element={<OrdersList />} />
+                <Route path="categories" element={<CategoriesList />} />
+                <Route path="users" element={<UsersList />} />
+            </Route>
         </Routes>
-
-
-
-
     );
-
 };
 
 export default AdminRoutes;
