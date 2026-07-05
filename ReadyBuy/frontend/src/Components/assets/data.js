3dcfc8b7e8e8f0f40c2600cf69ffed16064eb1b1
Anurag Kumar <ak7546824@gmail.com>
2026-07-05 20:36:52 +0530

    Create data.js

---
+++ ReadyBuy/frontend/src/Components/assets/data.js
@@ -0,0 +1,38 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product=[
+    {
+        id:1,
+        name:"Evergreen Tiered Skirt",
+        image:[p1_img],
+        new_price:3499,
+        old_price:4499,
+    },
+    {
+        id:2,
+        name:"Linen Stripe Utility Shirt",
+        image:[p2_img],
+        new_price:2999,
+        old_price:4099,
+    },
+    {
+        id:3,
+        name:"Aria V-Neck Midi Dress",
+        image:[p3_img],
+        new_price:4499,
+        old_price:4999,
+    },
+    {
+        id:4,
+        name:"Terracotta Grace Dress",
+        image:[p4_img],
+        new_price:4499,
+        old_price:5999,
+    }
+]
+
+export default data_product
+
