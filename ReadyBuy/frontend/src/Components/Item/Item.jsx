49f0b7a73a2feed65850995e9def19767d5f6fac
Anurag Kumar <ak7546824@gmail.com>
2026-07-06 14:45:44 +0530

    Create Item.jsx

---
+++ ReadyBuy/frontend/src/Components/Item/Item.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import "./Item.css";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+
+const Item = ({
+  slug,
+  image,
+  name,
+  new_price,
+  old_price,
+}) => {
+
+  const discount =
+    old_price > new_price
+      ? Math.round(
+        ((old_price - new_price) /
+          old_price) *
+        100
+      )
+      : 0;
+
+  return (
+    <div className="item">
+
+      <Link
+        to={`/product/${slug}`}
+        className="item-image"
+        onClick={() => window.scrollTo(0, 0)}
+      >
+
+        {discount > 0 && (
+          <span className="discount-badge">
+            -{discount}%
+          </span>
+        )}
+
+        <img
+          src={image || "/placeholder.png"}
+          alt={name}
+        />
+
+        <div className="item-overlay">
+          <span>
+            View Product
+            <FaArrowRight />
+          </span>
+        </div>
+
+      </Link>
+
+      <div className="item-content">
+
+        <h5 className="item-title">{name}</h5>
+
+        <div className="item-prices">
+
+          <span className="item-prices-new">
+            ₹{new_price}
+          </span>
+
+          {old_price > new_price && (
+            <span className="item-prices-old">
+              ₹{old_price}
+            </span>
+          )}
+
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Item;
