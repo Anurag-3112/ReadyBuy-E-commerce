8fbb17c47e73666e52fe668a59611484f1a78ec9
Anurag Kumar <ak7546824@gmail.com>
2026-07-06 16:57:37 +0530

    Create api.js

---
+++ ReadyBuy/frontend/src/services/api.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:5001/api/v1",
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem("accessToken");
+            localStorage.removeItem("refreshToken");
+            localStorage.removeItem("user");
+
+            window.location.replace("/login");
+        }
+
+        return Promise.reject(error);
+    }
+);
+
+export default api;
