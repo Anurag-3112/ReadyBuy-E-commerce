f47c7835434b71ae2636dcd74b79eed5abfddb66
Anurag Kumar <ak7546824@gmail.com>
2026-07-06 22:00:09 +0530

    Create auth.service.js

---
+++ ReadyBuy/frontend/src/services/auth.service.js
@@ -0,0 +1,27 @@
+import api from './api'
+
+export const registerUser = async (userData) => {
+    const res = await api.post(
+        '/auth/register',
+        userData
+    )
+
+    return res.data
+}
+
+export const loginUser = async (credentials) => {
+    const res = await api.post(
+        '/auth/login',
+        credentials
+    )
+
+    return res.data
+}
+
+export const getProfile = async () => {
+    const res = await api.get(
+        '/auth/profile'
+    )
+
+    return res.data
+}
