31d1e04420f99ccad3afaa2f31f440b8cc762ddc
Anurag Kumar <ak7546824@gmail.com>
2026-07-06 22:00:12 +0530

    Update product.admin.service.js

--- ReadyBuy/frontend/src/admin/services/product.admin.service.js
+++ ReadyBuy/frontend/src/admin/services/product.admin.service.js
@@ -1,36 +1,45 @@
 import api from "../../services/api";
 
-export const getProducts = async (params = {}) => {
+export const getProducts = async ({
+    page = 1,
+    search = "",
+}) => {
+
     const response = await api.get(
         "/products",
         {
-            params,
+            params: {
+                page,
+                limit: 10,
+                search,
+            },
         }
     );
 
     return response.data.data;
+
 };
 
 export const deleteProduct = async (
     id
 ) => {
+
     const response = await api.delete(
         `/products/${id}`
     );
 
     return response.data.data;
+
 };
 
-export const createProduct = async (formData) => {
+export const createProduct = async (
+    formData
+) => {
 
-    const response =
-        await api.post(
-
-            "/products",
-
-            formData
-
-        );
+    const response = await api.post(
+        "/products",
+        formData
+    );
 
     return response.data.data;
 
@@ -41,14 +50,10 @@ export const updateProduct = async (
     formData
 ) => {
 
-    const response =
-        await api.patch(
-
-            `/products/${id}`,
-
-            formData
-
-        );
+    const response = await api.patch(
+        `/products/${id}`,
+        formData
+    );
 
     return response.data.data;
 
