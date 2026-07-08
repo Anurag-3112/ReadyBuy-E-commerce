e4696a1a4dfd27f853ee45763ecb7fed8f034475
Anurag Kumar <ak7546824@gmail.com>
2026-07-08 23:45:27 +0530

    Update admin.service.js

--- ReadyBuy/frontend/src/admin/services/admin.service.js
+++ ReadyBuy/frontend/src/admin/services/admin.service.js
@@ -9,3 +9,41 @@ export const getMonthlyRevenue = async () => {
     const response = await api.get("/admin/revenue");
     return response.data.data;
 };
+
+export const getTopProducts = async () => {
+    const { data } = await api.get(
+        "/admin/top-products"
+    );
+
+    return data.data;
+};
+
+export const getTopCategories = async () => {
+    const { data } = await api.get(
+        "/admin/top-categories"
+    );
+
+    return data.data;
+};
+
+export const getRecentOrders = async () => {
+
+    const { data } =
+        await api.get(
+            "/admin/recent-orders"
+        );
+
+    return data.data;
+
+};
+
+export const getLowStockProducts = async () => {
+
+    const { data } =
+        await api.get(
+            "/admin/low-stock"
+        );
+
+    return data.data;
+
+};
