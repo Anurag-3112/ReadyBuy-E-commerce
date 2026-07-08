d11bcd0d1de5be1d6252865066058520c6ace596
Anurag Kumar <ak7546824@gmail.com>
2026-07-08 23:47:26 +0530

    Update admin.controller.js

--- ReadyBuy/backend/src/modules/admin/admin.controller.js
+++ ReadyBuy/backend/src/modules/admin/admin.controller.js
@@ -2,6 +2,10 @@ import asyncHandler from "../../shared/utils/asyncHandler.js";
 import {
     getDashboardStats,
     getMonthlyRevenue,
+    getTopProductsService,
+    getTopCategoriesService,
+    getRecentOrdersService,
+    getLowStockProductsService,
 } from "./admin.service.js";
 
 export const getStats =
@@ -34,3 +38,47 @@ export const monthlyRevenue =
             data,
         });
     });
+
+export const getTopProducts = asyncHandler(async (req, res) => {
+
+    const products = await getTopProductsService();
+
+    res.json({
+        success: true,
+        data: products,
+    });
+
+});
+
+export const getTopCategories = asyncHandler(async (req, res) => {
+
+    const categories = await getTopCategoriesService();
+
+    res.json({
+        success: true,
+        data: categories,
+    });
+
+});
+
+export const getRecentOrders = asyncHandler(async (req, res) => {
+
+    const orders = await getRecentOrdersService();
+
+    res.json({
+        success: true,
+        data: orders,
+    });
+
+});
+
+export const getLowStockProducts = asyncHandler(async (req, res) => {
+
+    const products = await getLowStockProductsService();
+
+    res.json({
+        success: true,
+        data: products,
+    });
+
+});
