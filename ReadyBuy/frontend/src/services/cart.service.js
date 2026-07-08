5b475e20c79425e5c159e1ba6a95f75d4f9fa17e
Anurag Kumar <ak7546824@gmail.com>
2026-07-08 23:47:13 +0530

    Update cart.service.js

--- ReadyBuy/frontend/src/services/cart.service.js
+++ ReadyBuy/frontend/src/services/cart.service.js
@@ -1,12 +1,22 @@
 import api from "./api";
 
-export const addToCart = async (productId) => {
-    const res = await api.post("/cart/add", {
-        productId,
-        quantity: 1,
-    });
+export const addToCart = async ({
+    productId,
+    size,
+    quantity = 1,
+}) => {
+
+    const res = await api.post(
+        "/cart",
+        {
+            productId,
+            size,
+            quantity,
+        }
+    );
 
     return res.data;
+
 };
 
 export const getCart = async () => {
@@ -16,20 +26,38 @@ export const getCart = async () => {
 
 export const updateCartQuantity = async (
     productId,
+    size,
     quantity
 ) => {
-    const res = await api.patch("/cart/update", {
-        productId,
-        quantity,
-    });
 
-    return res.data;
-};
-
-export const removeFromCart = async (productId) => {
-    const res = await api.delete(
-        `/cart/remove/${productId}`
+    const res = await api.patch(
+        "/cart/update",
+        {
+            productId,
+            size,
+            quantity,
+        }
     );
 
     return res.data;
+
+};
+
+export const removeFromCart = async (
+    productId,
+    size
+) => {
+
+    const res = await api.delete(
+        "/cart/remove",
+        {
+            data: {
+                productId,
+                size,
+            },
+        }
+    );
+
+    return res.data;
+
 };
