24aa52cf47a5facf66a3e7992919931f6ae83071
Anurag Kumar <ak7546824@gmail.com>
2026-07-08 23:47:24 +0530

    Update admin.routes.js

--- ReadyBuy/backend/src/modules/admin/admin.routes.js
+++ ReadyBuy/backend/src/modules/admin/admin.routes.js
@@ -6,10 +6,15 @@ import authorize from "../../shared/middleware/role.middleware.js";
 import {
     getStats,
     monthlyRevenue,
+    getTopProducts,
+    getTopCategories,
+    getRecentOrders,
+    getLowStockProducts,
 } from "./admin.controller.js";
 
 import orderRoutes from "../orders/order.routes.js";
 import userRoutes from "../users/user.routes.js";
+import couponRoutes from "../coupons/coupon.routes.js";
 
 const router = express.Router();
 
@@ -39,6 +44,41 @@ router.get(
     authenticate,
     authorize("ADMIN"),
     monthlyRevenue
+);
+
+router.use(
+    "/coupons",
+    authenticate,
+    authorize("ADMIN"),
+    couponRoutes
+);
+
+router.get(
+    "/top-products",
+    authenticate,
+    authorize("ADMIN"),
+    getTopProducts
+);
+
+router.get(
+    "/top-categories",
+    authenticate,
+    authorize("ADMIN"),
+    getTopCategories
+);
+
+router.get(
+    "/recent-orders",
+    authenticate,
+    authorize("ADMIN"),
+    getRecentOrders
+);
+
+router.get(
+    "/low-stock",
+    authenticate,
+    authorize("ADMIN"),
+    getLowStockProducts
 );
 
 export default router;
